8489ee38da83bd9c035282d2c6d6b75e3537e080
Ryan Reyes <ryan.david.reyes@gmail.com>
2016-03-22 23:13:35 -0400

    Added tcp driver, hyperterminal, and makefile.

---
+++ drivers/tcp/client.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include "tcpipnix.h"
+using namespace std;
+
+int main()
+{
+  //This client server implements a simple "chat" program.
+  //It sends one letter at a time.
+  //To talk to a server you need to do the following:
+  //Create the TCP Object
+  TCP tcpConn;
+
+  char input;
+
+  //Specify a port to connect to
+  int port = 12345;
+
+  string addr = "127.0.0.1";
+
+
+  //Specify an IP address such as the above
+  cout << "ip address:";
+  cin >> addr;
+
+  //run the connectToHost function, and pass in the port, and the address
+  //the function returns true when you connect, and false if you don't
+  cout << "connecting" << endl;
+  if (!tcpConn.connectToHost(port, addr.c_str()))
+    return 1;
+
+  cout << "connected" << endl;
+
+  while (1)
+  {
+    cin >> input;
+    //run the Senddata function, using tcpConn.getSocket() as the first argument
+    //a character pointer to the data for the second argument,
+    //and the size of the data to send (in bytes) (1char = 1byte)
+    tcpConn.sendData(tcpConn.getSocket(), (char *) &input, 1);
+    /*
+      char brake = 'B';
+      char throttle = 'T';
+      char gear = 'G';
+      int throttleVal, gearVal, brakeVal;
+
+
+      tcpConn.sendData(tcpConn.getSocket(), (char *) &brake, 1);
+      tcpConn.sendData(tcpConn.getSocket(), (char *) &brakeVal, 4);
+
+      tcpConn.sendData(tcpConn.getSocket(), (char *) &throttle, 1);
+      tcpConn.sendData(tcpConn.getSocket(), (char *) &throttleVal, 4);
+
+      tcpConn.sendData(tcpConn.getSocket(), (char *) &gear, 1);
+      tcpConn.sendData(tcpConn.getSocket(), (char *) &gearVal, 4);
+    */
+  }
+
+  return 0;
+}
---
+++ drivers/tcp/server.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "tcpipnix.h"
+using namespace std;
+
+int main()
+{
+  TCP tcpConn;
+  unsigned int commSock = 0;
+  char data;
+
+
+  tcpConn.listenToPort(12345);
+  commSock = tcpConn.acceptConnection();
+
+  while(1)
+  {
+    tcpConn.receiveData(commSock, (char *) &data);
+    cout << data << endl;
+  }
+
+  return 0;
+}
