db09c389903426889500e3720caa802731b124ee
Ryan Reyes <ryan.david.reyes@gmail.com>
2017-05-26 14:15:18 -0400

    Added velocity form of PID.

--- algo/pid/PID.c
+++ algo/pid/PID.c
@@ -1,10 +1,67 @@
 #include <PID.h>
 
-#define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+static float fdFirstDer(ring_buffer_t r_buff, float dt) {
+  const float A = -1.0/5.0;
+  const float B = 5.0/4.0;
+  const float C = -10.0/3.0;
+  const float D = 5;
+  const float E = -5;
+  const float F = 137.0/60.0;
 
-void setPIDConstants(pid_data_t *pid, const float proportional_gain,
-                     const float integral_gain, const float derivative_gain,
-                     const float integral_guard) {
+  //ring buffer, 0 is first.
+
+  float numerator =
+    A*rb_get(r_buff, 5) +
+    B*rb_get(r_buff, 4) +
+    C*rb_get(r_buff, 3) +
+    D*rb_get(r_buff, 2) +
+    E*rb_get(r_buff, 1) +
+    F*rb_get(r_buff, 0);
+
+  return numerator/dt;
+}
+
+static float fdSecondDer(ring_buffer_t r_buff, float dt) {
+  const float A = -5.0/6.0;
+  const float B = 61.0/12.0;
+  const float C = -13.0;
+  const float D = 107.0/6.0;
+  const float E = -77.0/6.0;
+  const float F = 15.0/4.0;
+
+  float numerator =
+    A*rb_get(r_buff, 5) +
+    B*rb_get(r_buff, 4) +
+    C*rb_get(r_buff, 3) +
+    D*rb_get(r_buff, 2) +
+    E*rb_get(r_buff, 1) +
+    F*rb_get(r_buff, 0);
+
+  return numerator/(dt*dt);
+}
+
+void pid_init(pid_data_t *pid, const float proportional_gain,
+    const float integral_gain, const float derivative_gain,
+    const float integral_guard) {
+  pid_setConstants(
+      pid,
+      proportional_gain,
+      integral_gain,
+      derivative_gain,
+      integral_guard);
+
+  //set up the ring buffers
+  rb_setBuffer(&pid->error_buffer_ring, pid->error_buffer);
+  rb_setBuffer(&pid->output_buffer_ring, pid->output_buffer);
+
+  //set contents of ring buffers to zero
+  rb_init(&pid->error_buffer_ring, PID_BUFFER_SIZE);
+  rb_init(&pid->output_buffer_ring, PID_BUFFER_SIZE);
+}
+
+void pid_setConstants(pid_data_t *pid, const float proportional_gain,
+    const float integral_gain, const float derivative_gain,
+    const float integral_guard) {
   pid->proportional_gain = proportional_gain;
   pid->integral_gain = integral_gain;
   pid->derivative_gain = derivative_gain;
@@ -13,7 +70,7 @@ void setPIDConstants(pid_data_t *pid, const float proportional_gain,
   pid->integral_error = 0;
 }
 
-void updatePID(pid_data_t *pid, const float *current_error, const float *delta_t) {
+void pid_update(pid_data_t *pid, const float *current_error, const float *delta_t) {
   float error_differential = 0;
 
   pid->integral_error += *current_error * (*delta_t);
@@ -22,15 +79,15 @@ void updatePID(pid_data_t *pid, const float *current_error, const float *delta_t
   error_differential = (*current_error - pid->previous_error)/(*delta_t);
 
   pid->pid_output = (pid->proportional_gain * (*current_error)) +
-                    (pid->integral_gain    * pid->integral_error) +
-                    (pid->derivative_gain  * error_differential);
+    (pid->integral_gain    * pid->integral_error) +
+    (pid->derivative_gain  * error_differential);
 
   pid->previous_error = *current_error;
 } //end pidControl()
 
 //fixed update PID is meant to be called at constant time intervals,
 //therefore it does not need delta_t
-void fixedUpdatePID(pid_data_t *pid, const float *current_error) {
+void pid_fixedUpdate(pid_data_t *pid, const float *current_error) {
   float error_differential = 0;
 
   pid->integral_error += *current_error;
@@ -40,8 +97,39 @@ void fixedUpdatePID(pid_data_t *pid, const float *current_error) {
   error_differential = (*current_error - pid->previous_error);
 
   pid->pid_output = (pid->proportional_gain * (*current_error)) +
-                    (pid->integral_gain    * pid->integral_error) +
-                    (pid->derivative_gain  * error_differential);
+    (pid->integral_gain    * pid->integral_error) +
+    (pid->derivative_gain  * error_differential);
 
   pid->previous_error = *current_error;
 } //end pidControl()
+
+void pid_velocUpdate(pid_data_t *pid, const float current_error,
+    const float dt) {
+  const float A = 18.0/11.0;
+  const float B = -9.0/11.0;
+  const float C = 2.0/11.0;
+  const float D = 6.0/11.0;
+
+  float Kp = pid->proportional_gain;
+  float Ki = pid->integral_gain;
+  float Kd = pid->derivative_gain;
+  struct ring_buffer_s *error_buffer = &pid->error_buffer_ring;
+  struct ring_buffer_s *output_buffer = &pid->output_buffer_ring;
+
+  rb_pushFront(error_buffer, current_error); //update the error buffer
+
+  float du =
+    Kp * fdFirstDer(error_buffer, dt) +
+    Ki*current_error +
+    Kd*fdSecondDer(error_buffer, current_error);
+
+  float u =
+    //this is the previous value since current val [n] not pushed
+    A*rb_get(output_buffer, 0) + //[n-1]
+    B*rb_get(output_buffer, 1) + //[n-2]
+    C*rb_get(output_buffer, 2) + //[n-3]
+    D*du*dt;
+
+  rb_pushFront(output_buffer, u); //update the error buffer
+  pid->pid_output = u;
+}
--- algo/pid/PID.h
+++ algo/pid/PID.h
@@ -1,5 +1,9 @@
 #ifndef PID_H_
 #define PID_H_
+#include <ring_buffer.h>
+
+#define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+#define PID_BUFFER_SIZE 6
 
 //Use this data structure to create a customized PID per motor
 typedef struct pid_data_t {
@@ -10,16 +14,36 @@ typedef struct pid_data_t {
   float integral_error;
   float integral_guard;
   float pid_output;
+
+  struct ring_buffer_s error_buffer_ring;
+  struct ring_buffer_s output_buffer_ring;
+
+  //private
+  float error_buffer[PID_BUFFER_SIZE];
+  float output_buffer[PID_BUFFER_SIZE];
 } pid_data_t;
 
-void setPIDConstants(pid_data_t *pid, const float proportional_gain,
-                     const float integral_gain, const float derivative_gain,
-                     const float integral_guard);
+void pid_setConstants(pid_data_t *pid,
+    const float proportional_gain,
+    const float integral_gain,
+    const float derivative_gain,
+    const float integral_guard);
 
-void updatePID(pid_data_t *pid,
+void pid_init(pid_data_t *pid,
+    const float proportional_gain,
+    const float integral_gain,
+    const float derivative_gain,
+    const float integral_guard);
+
+//traditional PID calculation
+void pid_update(pid_data_t *pid,
     const float *current_error,
     const float *delta_t);
 
-void fixedUpdatePID(pid_data_t *pid, const float *current_error);
+void pid_fixedUpdate(pid_data_t *pid, const float *current_error);
+
+//velocity PID implementation
+void pid_velocUpdate(pid_data_t *pid, const float current_error,
+    const float dt);
 
 #endif
