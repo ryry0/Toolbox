c5042f720d8ed8deaf3cdf9e58eac8dff37b0008
Ryan Reyes <ryan.david.reyes@gmail.com>
2017-06-05 20:58:17 -0400

    To backport: Cooperative schedule changes.
    (cherry picked from commit 024f47ec1b22226f222b642b955bbbc7b7725c53)

--- algo/coop/coop.h
+++ algo/coop/coop.h
@@ -1,7 +1,15 @@
 #ifndef COOP_H_
 #define COOP_H_
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-typedef void (*coop_task_func_t)(void);
+typedef struct coop_args_s {
+    volatile struct isr_struct_s *isr_shared_vars;
+    struct thread_struct_s *coop_thread_shared_vars;
+} coop_args_t;
+
+typedef void (*coop_task_func_t)(coop_args_t *);
 
 typedef struct coop_task_s {
   uint32_t period;
@@ -10,10 +18,16 @@ typedef struct coop_task_s {
   coop_task_func_t task_func;
 } coop_task_t;
 
-task_t* coop_getTaskList();
-void coop_scheduleTasks(coop_task_t *tasks);
-void coop_executeTasks(coop_task_t *tasks);
+
+typedef struct coop_task_list_s {
+  size_t num_tasks;
+  coop_task_t *task_arr;
+} coop_task_list_t;
+
+//task_t* coop_getTaskList();
+void coop_scheduleTasks(coop_task_list_t *tasks);
+void coop_executeTasks(coop_task_list_t *tasks, coop_args_t *args);
 size_t coop_getNumTasks();
-void coop_schedExecuteTasks(coop_task_t *tasks);
+void coop_schedExecuteTasks(coop_task_list_t *tasks, coop_args_t *args);
 
 #endif
