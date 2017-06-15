0ba02da812364c0087be47037f9171d58d89f535
Ryan Reyes <ryan.david.reyes@gmail.com>
2017-06-15 09:21:20 -0400

    Added numerical methods header file.

---
+++ algo/numerical.h
@@ -0,0 +1,18 @@
+/** \file
+ * \author Ryan-David Reyes
+ *
+ * \brief This file contains useful numerical methods.
+ */
+
+#ifndef NUMERICAL_H_
+#define NUMERICAL_H_
+
+/**
+ * \brief
+ *
+ *
+ * \param
+ */
+
+
+#endif
