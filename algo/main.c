a701c0e511d423d34c83f0d951047fc528874374
Ryan Reyes <ryan.david.reyes@gmail.com>
2017-06-15 15:11:26 -0400

    Simulation updates.

--- algo/main.c
+++ algo/main.c
@@ -9,23 +9,6 @@ float sinHertz(float freq, float time) {
   return sin(2*PI*freq*time);
 }
 
-size_t genRange(float start, float increment, float end, float **array) {
-  const size_t arr_size = ceil(fabs((end - start)/increment));
-  float *range = malloc(arr_size*sizeof(float));
-
-  for (size_t i = 0; i < arr_size; ++i) {
-    range[i] = start + i*increment;
-  }
-
-  *array = range;
-  return arr_size;
-}
-
-float expProcess(float prev, float setpoint, float sample_time, float tau) {
-  const float A = sample_time/tau;
-  return prev + A*setpoint/(A+1);
-}
-
 //tau is about .25*rise time.
 
 int main(int argc, char ** argv) {
@@ -36,9 +19,9 @@ int main(int argc, char ** argv) {
 
   pid_data_t pid;
 
-  pid_init(&pid, 1, 0, 0, 5000);
+  pid_init(&pid, 1.0, 0.1, 0.1, 5000);
 
-  size_t len_time = genRange(0, dt, 10.0, &time);
+  size_t len_time = nm_genRange(0, dt, 100.0, &time);
 
   printf("t,s,p,c\n");
   for (size_t i = 0; i < len_time; ++i) {
@@ -46,14 +29,15 @@ int main(int argc, char ** argv) {
     float commanded_setpoint = 0;
     static float sensor_prev = 0;
     static float pid_output = 0;
+    float tau = 10; //0.00025
 
     if (time[i] > 2.0)
       commanded_setpoint = set;
 
-    float sensor = expProcess(sensor_prev, pid_output, dt, 0.00025);
+    float sensor = nm_expProcess(sensor_prev, pid_output, dt, tau);
 
     pid_output = pid_FeedbackCtrl(&pid, commanded_setpoint, sensor, dt,
-      pid_minUpdate);
+        pid_velocUpdate);
 
     printf("%f,%f,%f,%f\n", time[i], commanded_setpoint, sensor, pid_output);
 
