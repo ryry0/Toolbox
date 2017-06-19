b8a4257dcfb2b06f1c88978109587b9b5fe23a6c
Ryan Reyes <ryan.david.reyes@gmail.com>
2017-06-19 10:32:43 -0400

    To backport: Doxygen'ed cobs.h.
    (cherry picked from commit 489f02d1136de6484cb2153bf18583f9dc527493)

--- algo/cobs.h
+++ algo/cobs.h
@@ -1,3 +1,9 @@
+/** \file
+ *
+ * \author Ryan-David Reyes
+ *
+ * \brief This is an implementation of the COBS packet framing algorithm.
+ */
 #ifndef COBS_H_
 #define COBS_H_
 
@@ -5,37 +11,58 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-/*
-   cobs Encode in place does the encoding on the same array passed in, and does
-   not require using a different array. This may be preferable for performance
-   reasons. Encode in place assumes that the first byte is the header byte and
-   the second byte is the code byte.
-   */
+/**
+ * \brief Performs COBS encoding on the array passed in.
+ *
+ * Does not require using a different array. This may be preferable for performance
+ * reasons. Encode in place assumes that the first byte is the header byte and
+ * the second byte is the code byte.
+ *
+ * \param buffer Array to perform encoding on.
+ * \param length Length of the array.
+ */
 void cobs_encodeInPlace(uint8_t* buffer, const uint8_t total_length);
 
-/*
-   For destination, pass in an array that is 2 bytes larger than the source
-   length.
-   */
+/**
+ * \brief Takes bytes from source, performs COBS encoding, places it in
+ * destination.
+ *
+ * For destination buffer, pass in an array that is 2 bytes larger than the source
+ * length.
+ *
+ * \param source Source array.
+ * \param length Length of source array.
+ * \param destination Destination array.
+ */
 void cobs_encode(const uint8_t* source,
     const uint8_t length,
     uint8_t* destination);
 
 
-/*
-   Decode in place does the decoding on the same array passed in. Decode in place
-   assumes that the first byte is the header byte and the second byte is the code
-   byte.
-   */
+/**
+ * \brief Decode in place does the decoding on the array passed in.
+ *
+ * Decode in place assumes that the first byte is the header byte and the second
+ * byte is the code byte.
+ *
+ * \param buffer Array to perform decoding on.
+ * \param length Length of the array.
+ */
 void cobs_decodeInPlace(uint8_t* buffer,
     const uint8_t length);
 
 
-/*
-   For destination, pass in an array that is at least as large as the source
-   length minus 2.
-   Strips the  header and code byte from the data.
-   */
+/**
+ * \brief Takes data from source, decodes it, and places it in destination.
+ *
+ * For destination, pass in an array that is at least as large as the source
+ * length minus 2.
+ * Strips the  header and code byte from the data.
+ *
+ * \param source Source array.
+ * \param length Length of source array.
+ * \param destination Destination array.
+ */
 void cobs_decode(const uint8_t* source,
     const uint8_t length,
     uint8_t* destination);
